e073fa040d1cf4bee31cbd7bd900044552d4973b
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-25 11:02:05 -0300

    style: css da pagina login concluida

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -31,24 +31,41 @@ function Login() {
   }
 
   return (
-    <div>
-      <img src="/src/images/logo.svg" alt="logo da aplicação" />
-      <form onSubmit={ handleSubmitForm }>
-        <input
-          type="text"
-          data-testid="login-name-input"
-          placeholder="Qual é o seu nome?"
-          value={ inputValue }
-          onChange={ handleChangeLogin }
+    <section
+      className="bg-login bg-no-repeat bg-cover h-screen flex justify-center items-center"
+    >
+      <div
+        className="
+        bg-white w-[46.875rem] h-[28.125rem] rounded-2xl flex flex-col
+        items-center"
+      >
+        <img
+          src="/src/images/logo.svg"
+          alt="logo da aplicação"
+          className="w-[11.688rem] h-[6.563rem] mt-28"
         />
-        <button
-          disabled={ inputValue.length < 3 }
-          data-testid="login-submit-button"
-        >
-          Entrar
-        </button>
-      </form>
-    </div>
+        <form onSubmit={ handleSubmitForm } className="flex flex-col mt-14 gap-2.5">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            placeholder="Qual é o seu nome?"
+            value={ inputValue }
+            onChange={ handleChangeLogin }
+            className="
+            w-[25rem] h-10 text-center rounded-full border border-login-blue
+            text-login-blue placeholder:text-login-blue placeholder:text-sm font-normal"
+          />
+          <button
+            disabled={ inputValue.length < 3 }
+            data-testid="login-submit-button"
+            className="rounded-full bg-login-blue w-[25rem] h-10 uppercase text-white
+            font-bold text-center disabled:bg-gray-400 disabled:cursor-not-allowed"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login': "url('/src/images/login.svg')",
+      },
+      colors: {
+        'login-blue': '#003BE5',
+      },
+      fontFamily: {
+        'sans': ['"Epilogue"', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
