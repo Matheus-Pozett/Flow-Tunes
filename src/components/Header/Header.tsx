ec9119bb6f70188d48368c3b2f7a064ba2f89744
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-25 14:54:11 -0300

    style: css do header concluido

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { USER_VALUES, UserType } from '../../types';
 import { getUser } from '../../services/userAPI';
-import { Loading } from '../Loading';
+import './header.css';
 
 function Header() {
   const [username, setUsername] = useState<UserType>(USER_VALUES);
@@ -23,19 +23,54 @@ function Header() {
   }, []);
 
   return (
-    <header data-testid="header-component">
-      {loading ? (<Loading />
+    <header
+      data-testid="header-component"
+      className="w-[15.625rem] h-screen flex flex-col py-8 px-10 justify-between"
+    >
+      <img
+        src="/src/images/logo.svg"
+        alt="Logo da aplicação"
+        className="w-[10.625rem] h-24"
+      />
+      <nav className="flex flex-col gap-14">
+        <NavLink
+          to="/search"
+          data-testid="link-to-search"
+          className="flex gap-4 text-[#444955]"
+        >
+          <img src="/src/images/Vector.svg" alt="icone da pesquisa" />
+          Pesquisa
+        </NavLink>
+
+        <NavLink
+          to="/favorites"
+          data-testid="link-to-favorites"
+          className="flex gap-4 text-[#444955]"
+        >
+          <img src="/src/images/icon_starempty.svg" alt="icona de musicas favoritas" />
+          Favoritas
+        </NavLink>
+
+        <NavLink
+          to="/profile"
+          data-testid="link-to-profile"
+          className="flex gap-4 text-[#444955]"
+        >
+          <img src="/src/images/icon_starempty.svg" alt="icone do perfil" />
+          Perfil
+        </NavLink>
+      </nav>
+
+      {loading ? (
+        <div className="flex gap-8">
+          <span className="loader-header" />
+          <p>Carregando...</p>
+        </div>
       ) : (
-        <>
-          <img src="/src/images/logo.svg" alt="Logo da aplicação" />
-          <nav>
-            <NavLink to="/search" data-testid="link-to-search">Pesquisa</NavLink>
-            <NavLink to="/favorites" data-testid="link-to-favorites">Favoritas</NavLink>
-            <NavLink to="/profile" data-testid="link-to-profile">Perfil</NavLink>
-          </nav>
-          <p data-testid="header-user-name">{username.name}</p>
-        </>
-      )}
+        <div className="flex items-center gap-3">
+          <img src="/src/images/foto.png" alt="" />
+          <p>{username.name}</p>
+        </div>)}
     </header>
   );
 }
