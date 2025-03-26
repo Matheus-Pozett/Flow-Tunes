a58f4760bb65e7002bab54f4e17947561f94c3e5
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-26 15:23:47 -0300

    style: css da pagina profile em andamento

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -27,25 +27,42 @@ function Profile() {
   }
 
   return (
-    <div>
-      <img
-        src={ user.image }
-        alt="Foto de perfil do usuario"
-        data-testid="profile-image"
-      />
-      <div>
-        <p>Nome</p>
-        <p>{user.name}</p>
+    <div className="flex flex-col flex-1 items-center bg-gray-100 min-h-screen">
+      <div className="w-full h-56 bg-loading relative">
+        <div className="absolute top-10 left-1/2 transform -translate-x-1/2">
+          <img
+            src={ user.image }
+            alt="Foto de perfil do usuário"
+            data-testid="profile-image"
+            className="w-32 h-32 rounded-full border-4 border-white shadow-lg"
+          />
+        </div>
       </div>
-      <div>
-        <p>E-mail</p>
-        <p>{user.email}</p>
+
+      {/* Conteúdo do perfil */}
+      <div className="mt-20 bg-white shadow-lg rounded-lg p-2 w-3/4 max-w-lg text-center">
+        <div className="text-gray-800">
+          <p className="text-lg font-semibold">Nome</p>
+          <p className="text-gray-500">{user.name}</p>
+        </div>
+
+        <div className="mt-4 text-gray-800">
+          <p className="text-lg font-semibold">E-mail</p>
+          <p className="text-gray-500">{user.email}</p>
+        </div>
+
+        <div className="mt-4 text-gray-800">
+          <p className="text-lg font-semibold">Descrição</p>
+          <p className="text-gray-500 text-sm">{user.description}</p>
+        </div>
+
+        <Link
+          to="/profile/edit"
+          className="mt-6 inline-block bg-blue-600 text-white px-6 py-2 rounded-full font-semibold shadow-md hover:bg-blue-700 transition"
+        >
+          Editar perfil
+        </Link>
       </div>
-      <div>
-        <p>Descrição</p>
-        <p>{user.description}</p>
-      </div>
-      <Link to="/profile/edit">Editar perfil</Link>
     </div>
   );
 }
